f6be7b8ca7670a4f0b6470d6b0f8f861751e097c
ArrowMin <botbot_aromin@yahoo.com>
2024-01-04 20:49:50 -0800

    implement basic functioning

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const divide = function (a, b) {
 };
 
 const exponential = function (a, b) {
-  return a ^ b;
+  return a ** b;
 };
 
 const negate = function (a) {
@@ -26,9 +26,70 @@ const operate = function (a, b, operator) {
   return operator(a, b);
 };
 
+const display = document.querySelector(".displaybox")
+
 const buttons = document.querySelectorAll(".button");
+
+let term1 = "", term2 = "", operator = "";
+
 buttons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    alert(btn.innerText);
-  });
+    btn.addEventListener("click", () => {
+        if (btn.classList.contains("clearbutton")) {
+            display.innerText = "";
+            term1 = "";
+            term2 = "";
+        }
+        //if number
+        if (btn.classList.contains("number")) {
+            //if term1 inputted already
+            if (operator != "") {
+                display.innerText = "";
+            }
+            display.innerText += btn.innerText;
+        }
+        //if operator
+        if (btn.classList.contains("operater")) {
+            term1 = display.innerText
+            //if no first number submitted
+            if (term1 === "") {
+                console.log("empty")
+                return;
+            }
+            operator = btn.innerText;
+            display.innerText += btn.innerText;
+        }
+
+        //if equals
+        if (btn.classList.contains("equals")) {
+            if (term1 != "" && operator != "") {
+                term2 = display.innerText;
+            }
+            //if both terms filled out
+            if (term1 != "" && term2 != "" && operator != "") {
+                console.log("made it");
+                let operation;
+                switch (operator) {
+                    case "^":
+                        operation = exponential;
+                        break;
+                    case "÷":
+                        operation = divide;
+                        break;
+                    case "×":
+                        operation = multiply;
+                        break;
+                    case "−":
+                        operation = subtract;
+                        break;
+                    case "+":
+                        operation = add;
+                        break;
+                    case "(-)":
+                        operation = negate;
+                        break;
+                }
+                display.innerText = operate(parseInt(term1), parseInt(term2), operation);
+            }
+        }
+    });
 });
